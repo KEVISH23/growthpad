b9f0954d04df89ca1a6b67ac038dc2a636a63b17
KEVISH23 <kevishthakkar001@gmail.com>
2022-12-06 14:33:32 +0530

    hh

--- src/components/BeforeLoginHomePage.js
+++ src/components/BeforeLoginHomePage.js
@@ -5,11 +5,13 @@ import "../Css/HomePage.css"
 const BeforeLoginHomePage = () => {
   return (
     <>
-      <Grid container>
-        <Grid item lg={12} md={12} sm={12} xs={12}>
-          <img className="image" src="./images/bHome.jpg" alt="" />
-        </Grid>
-      </Grid>
+      <div className='homeImage'></div>
+        <div className='image'>
+
+          <button>JIII</button>
+        </div>
+         
+        
     </>
   );
 }
