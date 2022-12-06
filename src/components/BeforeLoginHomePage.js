612b3ea0093b77005e8812df8eed4d39b26bdb68
Sarthak0011 <72193917+Sarthak0011@users.noreply.github.com>
2022-12-06 15:03:47 +0530

    Finally

--- src/components/BeforeLoginHomePage.js
+++ src/components/BeforeLoginHomePage.js
@@ -1,14 +1,14 @@
-import { Grid } from '@mui/material'
-import { Container } from '@mui/system'
+import { Button } from '@mui/material'
+
 import React from 'react'
 import "../Css/HomePage.css"
 const BeforeLoginHomePage = () => {
   return (
     <>
-      <div className='homeImage'></div>
+      
         <div className='image'>
 
-          <button>JIII</button>
+          <Button variant='contained'>JIII</Button>
         </div>
          
         
