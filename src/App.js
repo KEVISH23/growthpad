1b4d2d5c052cd979838995441b7d5729f8b339a5
Sarthak0011 <72193917+Sarthak0011@users.noreply.github.com>
2022-12-07 16:33:43 +0530

    Merge branch 'master' of https://github.com/KEVISH23/growthpad

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import BeforeLoginHomePage from "./components/BeforeLoginHomePage";
 import Login from "./components/Login";
-
+import HomeForAll from "./components/HomeForAll";
 function App() {
   return (
     <>
+    <HomeForAll/>
     {/* <BeforeLoginHomePage/> */}
-    <Login></Login>
+    {/* <Login></Login> */}
     </>
   );
 }
